ca04bf4c87755127182475ec1a4a051d83b30722
СадыковаЖ <@205_8>
2019-02-20 12:02:40 +0600

    Add project files.

---
+++ MedOrgWeb1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Med.Egov.Lib.Model;
+namespace MedOrgWeb1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //MedOrg m = new MedOrg();
+            //m.Address = "";
+            //m.Name = "Clinic 3";
+
+            //ServiceMedOrg.AddMedOrg(m);
+
+            foreach (MedOrg item in ServiceMedOrg.GetMedOrg())
+            {
+                Console.WriteLine(item.Name);
+            }
+        }
+    }
+}
